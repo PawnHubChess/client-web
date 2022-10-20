e793f38101e1794c10b08249c6f997deda194a68
Florian König <koenidv@gmail.com>
2022-10-20 14:48:52 +0200

    Move to SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Counter from '../lib/Counter.svelte';
+</script>
+
+<main>
+	<h1>Welcome to PawnHub</h1>
+
+	<div class="card">
+		<Counter />
+	</div>
+
+	<p>
+		Check out <a
+			href="https://github.com/PawnHubChess/backend/wiki"
+			rel="noreferrer"
+			target="_blank">backend wiki</a
+		>!
+	</p>
+</main>
+
+<style>
+	.logo {
+		height: 6em;
+		padding: 1.5em;
+		will-change: filter;
+	}
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+	.logo.svelte:hover {
+		filter: drop-shadow(0 0 2em #ff3e00aa);
+	}
+	.read-the-docs {
+		color: #888;
+	}
+</style>
