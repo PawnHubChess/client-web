cf3c603cf1bb6074e96c2e3f7c40de8b77901a8d
Florian König <koenidv@gmail.com>
2022-10-20 15:29:56 +0200

    Overengineer error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="image" style="background-image: url('https://http.cat/{$page.status}.jpg');">
+	<h1>Error {$page.status}: {$page.error?.message}</h1>
+</div>
+
+<style>
+	.image {
+		position: fixed;
+		top: 0;
+		left: 0;
+		background-position: center;
+		background-repeat: repeat;
+		height: 100vh;
+		width: 100vw;
+	}
+
+	.image h1 {
+    width: fit-content;
+    padding: 2rem;
+		color: rgba(255, 255, 255, 0.87);
+		background-color: #b93030;
+    margin-top: 50vh;
+    margin-left: 50vw;
+    transform: translateY(-50%) translateX(-50%);
+	}
+</style>
