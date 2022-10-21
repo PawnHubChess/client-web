323a0c2f2687769a7ac8a5b4c44290cba9d16d4d
Florian König <koenidv@gmail.com>
2022-10-21 08:31:04 +0200

    Host client receive messages

--- src/lib/chess/host.ts
+++ src/lib/chess/host.ts
@@ -2,6 +2,7 @@ import { get, writable } from "svelte/store";
 
 let ws: WebSocket;
 export const hostRunningState = writable(false);
+export const hostId = writable();
 
 export function startHost(
   useProd: boolean,
@@ -22,11 +23,22 @@ export function startHost(
     callback("Host Websocket closed");
     hostRunningState.set(false);
   });
+
+  ws.addEventListener("message", message => {
+    callback("<< " + message.data)
+    const data = JSON.parse(message.data)
+
+    if (data.type === "connected-id") {
+        callback(`Host ID is ${data.id}`)
+        hostId.set(data.id)
+    }
+  })
 }
 
 export function stopHost() {
   ws.close();
   hostRunningState.set(false);
+  hostId.set(undefined)
 }
 
 export function sendJson(message: string, callback: (output: string) => void) {
