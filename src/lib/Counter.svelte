436ee4f82af366ceeeb62a94298be3f349fbe4ec
Florian König <koenidv@gmail.com>
2022-10-31 13:01:18 +0100

    Add confetti to counter

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+	import { tick } from 'svelte';
+	import { ConfettiExplosion } from 'svelte-confetti-explosion';
+
+	let count: number = 0;
+	let confettiVisible = false;
+
+	const increment = async () => {
+		count += 1;
+		if (count % 10 === 0) {
+			confettiVisible = false;
+			await tick();
+			confettiVisible = true;
+		}
+	};
 </script>
 
 <button on:click={increment}>
-  count is {count}
+	count is {count}
+  {#if confettiVisible}
+    <ConfettiExplosion particleCount={count} force={count >= 1000? 1 : count * 0.001} />
+  {/if}
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 
 	<div class="card">
 		<Counter />
+		
 	</div>
 
 	<p>
