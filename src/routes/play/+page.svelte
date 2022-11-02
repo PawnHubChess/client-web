fdc78ab4f09eb4bb7a5cbd73fa75805d983c31fd
Florian König <koenidv@gmail.com>
2022-11-02 16:21:39 +0100

    Add connect button

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,110 +1,129 @@
-<div class="flex flex-col gap-6 mt-16">
-<div class="flex justify-center items-center space-x-10">
-	<div class="grid grid-cols-4 gap-4 w-fit">
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-1"
-				id="code-1-1"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
+<div class="flex flex-col mt-16 items-center">
+	<div class="flex flex-col gap-6 w-fit">
+		<div class="flex justify-center items-center space-x-10">
+			<div class="grid grid-cols-4 gap-4 w-fit">
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-1"
+						id="code-1-1"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-2"
+						id="code-1-2"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-3"
+						id="code-1-3"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-4"
+						id="code-1-4"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+			</div>
 		</div>
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-2"
-				id="code-1-2"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
+
+		<div class="flex justify-center items-center space-x-10">
+			<div class="grid grid-cols-4 gap-4 w-fit">
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-1"
+						id="code-1-1"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-2"
+						id="code-1-2"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-3"
+						id="code-1-3"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+				<div class="relative mt-1 rounded-md shadow-sm w-fit">
+					<input
+						type="number"
+						name="code-1-4"
+						id="code-1-4"
+						min="0"
+						max="9"
+						class="block text-center text-5xl font-bold text-bold rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
+						placeholder=""
+					/>
+				</div>
+			</div>
 		</div>
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-3"
-				id="code-1-3"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
+
+		<a
+			class="text-base mt-2 text-center font-medium text-indigo-600 rounded-md bg-indigo-100 px-4 py-2 hover:bg-indigo-600 hover:text-white transition-all"
+			href="/play/create"
+		>
+			Connect
+		</a>
+
+		<div class="relative py-4">
+			<div class="absolute inset-0 flex items-center justify-center">
+				<div class="w-80 border-b border-gray-300" />
+			</div>
+			<div class="relative flex justify-center">
+				<span class="bg-white px-4 text-sm text-gray-500">or</span>
+			</div>
 		</div>
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-4"
-				id="code-1-4"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
-		</div>
+
+		<a
+			class="text-base text-center font-medium text-indigo-600 rounded-md bg-indigo-100 px-4 py-2 hover:bg-indigo-600 hover:text-white transition-all"
+			href="/play/create"
+		>
+			Start a New Game
+		</a>
+
+		<a
+			class="text-base text-center font-medium text-gray-500 hover:text-gray-900 rounded-md"
+			href="/play/debug">Debug Page</a
+		>
 	</div>
-</div>
-
-<div class="flex justify-center items-center space-x-10">
-	<div class="grid grid-cols-4 gap-4 w-fit">
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-1"
-				id="code-1-1"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
-		</div>
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-2"
-				id="code-1-2"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
-		</div>
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-3"
-				id="code-1-3"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
-		</div>
-		<div class="relative mt-1 rounded-md shadow-sm w-fit">
-			<input
-				type="number"
-				name="code-1-4"
-				id="code-1-4"
-				min="0"
-				max="9"
-				class="block text-center text-5xl font-bold text-bold w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-[1.2em]"
-				placeholder=""
-			/>
-		</div>
-	</div>
-</div>
-
-
-
-<div class="flex flex-col justify-center items-center mt-4">
-	<a class="text-base font-medium text-indigo-600 rounded-md bg-indigo-100 px-4 py-2 hover:bg-indigo-600 hover:text-white transition-all" href="/play/create">
-		Start a New Game
-	</a>
-    
-	<a class="text-base font-medium text-gray-600 hover:text-gray-900 rounded-md px-4 py-2" href="/play/debug">Debug Page</a>
-</div>
-
 </div>
