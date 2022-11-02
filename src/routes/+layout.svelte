6c36f4e8ef21bcc2135880b4667b203a1f5168fe
Florian König <koenidv@gmail.com>
2022-11-02 16:34:10 +0100

    Max width on page content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,6 +33,6 @@
 		  <a href="/play" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Play Chess!</a>
 		</div>
 	  </div>
+	  
+	  <slot />
 	</div>
-
-<slot />
