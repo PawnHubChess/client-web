8c22b5adaaeb3287e2088f645bbdd4743e5a84e3
Florian König <koenidv@gmail.com>
2022-11-02 14:32:48 +0100

    Recreate nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,37 @@
     import "../app.css";
 </script>
 
-<nav>
-	<a href="/" class="text-3xl font-bold underline">PawnHub</a>
-	<a href="/project">The Project</a>
-	<a href="/team">Meet the Team</a>
-	<a href="/play">Play Chess!</a>
-</nav>
+	<div class="mx-auto max-w-7xl px-4 sm:px-6">
+	  <div class="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+		<div class="flex justify-start lg:w-0 lg:flex-1">
+		  <a href="/">
+			<span class="sr-only">PawnHub</span>
+			<img class="h-8 w-auto sm:h-10" src="https://www.redwolf.in/image/cache/catalog/marketplace/Samay%20Raina/pawn-hub-black-t-shirt-700x700.jpg" alt="">
+		  </a>
+		</div>
+		<div class="-my-2 -mr-2 md:hidden">
+		  <button type="button" class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+			<span class="sr-only">Open menu</span>
+			<!-- Heroicon name: outline/bars-3 -->
+			<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+			  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+			</svg>
+		  </button>
+		</div>
+		<nav class="hidden space-x-10 md:flex">
+  
+		  <a href="/" class="text-base font-medium text-gray-500 hover:text-gray-900">The Project</a>
+		  <a href="/team" class="text-base font-medium text-gray-500 hover:text-gray-900">Meet the Team</a>
+		  <a href="/research" class="text-base font-medium text-gray-500 hover:text-gray-900">Research</a>
+		  <a href="/art" class="text-base font-medium text-gray-500 hover:text-gray-900">Connecting Cultures</a>
+  
+		  <div class="relative">
+		  </div>
+		</nav>
+		<div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+		  <a href="/play" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Play Chess!</a>
+		</div>
+	  </div>
+	</div>
 
 <slot />
