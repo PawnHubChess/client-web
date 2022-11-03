5d62596ffed76d5ada65a774eb0999cf704db788
Florian König <koenidv@gmail.com>
2022-11-03 23:43:16 +0100

    Use chessboard-element to render chessboard

--- src/routes/play/game/+page.svelte
+++ src/routes/play/game/+page.svelte
@@ -3,13 +3,29 @@
 	import { playstate, client_id, reconnect_code } from '$lib/store';
 	import { onMount } from 'svelte';
 	import { get } from 'svelte/store';
+	import 'chessboard-element';
 
 	onMount(async () => {
-		if (get(playstate) !== 'playing') goto('/play');
+		//if (get(playstate) !== "playing") goto("/play");
+
+		let board = document.querySelector('chess-board');
+		board?.start();
+
+		board?.addEventListener('drop', (e) => {
+			// @ts-ignore
+			const { source, target } = e.detail;
+			console.log(`${source} -> ${target}`);
+		});
 	});
 </script>
 
-<p class="text-base text-gray-600 font-medium text-center mt-12">
+<div class="flex justify-center mt-8">
+	<chess-board draggable-pieces style="width: 80vh" />
+</div>
+
+<p class="text-base text-gray-600 font-medium text-center">
 	You're matched to another player. Your id is <span class="font-bold">{$client_id}</span>
 </p>
-<p class="text-base text-gray-500 font-medium text-center mt-4">Reconnect Code: {$reconnect_code}</p>
+<p class="text-base text-gray-500 font-medium text-center mt-4">
+	Reconnect Code: {$reconnect_code}
+</p>
