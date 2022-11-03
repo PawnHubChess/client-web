7f8cc319f01fab6a72110cc669214e4ebb2e9eb0
Florian König <koenidv@gmail.com>
2022-11-03 16:57:00 +0100

    Create Game Page

---
+++ src/routes/play/create/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { hostClient, host_code } from '$lib/chess/HostClient';
+	import { client_id } from '$lib/store';
+
+	if (hostClient().state === WebSocket.CLOSED) {
+		hostClient().connect();
+	}
+</script>
+
+<div class="flex flex-col justify-center items-center gap-10 mt-16">
+	<p class="text-7xl text-gray-900 font-bold">{$client_id}</p>
+	<p class="text-7xl text-gray-900 font-bold">{$host_code}</p>
+	<p class="text-base text-gray-600 font-medium">Waiting for Opponent</p>
+</div>
