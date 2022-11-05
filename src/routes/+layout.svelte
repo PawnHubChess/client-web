e97eb74f5e664e7f9d6881f4e13e155d536a5cdb
Florian König <koenidv@gmail.com>
2022-11-05 21:22:10 +0100

    Remove mobile menu focus ring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,7 +57,7 @@
 			<button
 				on:click={toggleMenu}
 				type="button"
-				class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-600 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+				class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900"
 				aria-expanded="false">
 				<span class="sr-only">Open menu</span>
 				<svg
