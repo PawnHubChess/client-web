a4eff993e83c2a5aa202c7d2eac8a46d28c112ad
Florian König <koenidv@gmail.com>
2022-11-05 21:18:44 +0100

    Mobile Menu Icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,10 +57,9 @@
 			<button
 				on:click={toggleMenu}
 				type="button"
-				class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+				class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-600 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
 				aria-expanded="false">
 				<span class="sr-only">Open menu</span>
-				<!-- Heroicon name: outline/bars-3 -->
 				<svg
 					class="h-6 w-6"
 					xmlns="http://www.w3.org/2000/svg"
@@ -68,11 +67,24 @@
 					viewBox="0 0 24 24"
 					stroke-width="1.5"
 					stroke="currentColor"
-					aria-hidden="true">
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+					aria-hidden="true"
+					aria-labelledby="ariaMenuTitle">
+					<title id="ariaMenuTitle">{!openMenu ? "Open Menu" : "Close Menu"}</title>
+					{#if !openMenu}
+						<path
+							d="M9,11H5c-1.105,0-2-0.895-2-2V5c0-1.105,0.895-2,2-2h4c1.105,0,2,0.895,2,2v4C11,10.105,10.105,11,9,11z" /><path
+							d="M19,11h-4c-1.105,0-2-0.895-2-2V5c0-1.105,0.895-2,2-2h4c1.105,0,2,0.895,2,2v4 C21,10.105,20.105,11,19,11z"
+							opacity=".35" /><path
+							d="M9,21H5c-1.105,0-2-0.895-2-2v-4c0-1.105,0.895-2,2-2h4c1.105,0,2,0.895,2,2v4 C11,20.105,10.105,21,9,21z"
+							opacity=".35" /><path
+							d="M19,21h-4c-1.105,0-2-0.895-2-2v-4c0-1.105,0.895-2,2-2h4c1.105,0,2,0.895,2,2v4C21,20.105,20.105,21,19,21z" />
+					{:else}
+						<path
+							d="M18,21H6c-1.657,0-3-1.343-3-3V6c0-1.657,1.343-3,3-3h12c1.657,0,3,1.343,3,3v12	C21,19.657,19.657,21,18,21z"
+							opacity=".35" /><path
+							d="M14.812,16.215L7.785,9.188c-0.384-0.384-0.384-1.008,0-1.392l0.011-0.011c0.384-0.384,1.008-0.384,1.392,0l7.027,7.027	c0.384,0.384,0.384,1.008,0,1.392l-0.011,0.011C15.82,16.599,15.196,16.599,14.812,16.215z" /><path
+							d="M7.785,14.812l7.027-7.027c0.384-0.384,1.008-0.384,1.392,0l0.011,0.011c0.384,0.384,0.384,1.008,0,1.392l-7.027,7.027	c-0.384,0.384-1.008,0.384-1.392,0l-0.011-0.011C7.401,15.82,7.401,15.196,7.785,14.812z" />
+					{/if}
 				</svg>
 			</button>
 		</div>
