889b926a44eb5e8571cba8e3007e9aa7d25b6d0d
Florian König <koenidv@gmail.com>
2022-11-06 01:15:42 +0100

    Create connect help page

---
+++ src/routes/play/help/+page.svelte
@@ -0,0 +1,9 @@
+<main class="mt-2 flex flex-col items-center tall:mt-8">
+	<h5 class="mt-4 text-lg font-bold text-gray-800">Connecting to the Chess Board</h5>
+	<p class="mt-2 text-base font-medium text-gray-800">There will be some text here...</p>
+	<h5 class="mt-8 text-lg font-bold text-gray-800">Hosting a Game</h5>
+	<p class="mt-2 text-base font-medium text-gray-800">...</p>
+	<h5 class="mt-8 text-lg font-bold text-gray-800">Report an Issue</h5>
+	<p class="mt-2 text-base font-medium text-gray-800">...</p>
+	<a href="/play" class="mt-8 text-base font-medium text-indigo-600 underline">Go Back</a>
+</main>
