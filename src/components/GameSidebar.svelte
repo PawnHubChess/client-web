eee8bf8ae87522271e24fe146fa9f98f39445d90
Florian König <koenidv@gmail.com>
2022-11-07 17:03:55 +0100

    SR Keyboard-Controlled Move Input Element & Style

--- src/components/GameSidebar.svelte
+++ src/components/GameSidebar.svelte
@@ -23,6 +23,17 @@
 			<Diamonds color="#4d5536" size="24" duration="2s" />
 		{/if}
 	</p>
+
+	<div class="sr-focus-layout rounded-md bg-gray-100 p-2">
+		<p class="mb-3 text-base text-gray-600">Make a move. {isOwnMove ? "It's your turn." : "It's your opponents turn."}</p>
+		<label for="move-from">Move from:</label>
+		<input type="text" id="move-from" class="rounded-md mt-1 mb-2" maxlength="2" />
+		<label for="move-to">Move to:</label>
+		<input type="text" id="move-to" class="rounded-md mt-1" maxlength="2" />
+		<input type="submit" value="Submit Move" class="button-secondary mt-4 w-full" />
+	</div>
+
 	<button class="button-secondary" on:click={handleLeaveGame}>Refresh</button>
 	<button class="button-secondary" on:click={handleLeaveGame}>Leave Game</button>
+	
 </div>
