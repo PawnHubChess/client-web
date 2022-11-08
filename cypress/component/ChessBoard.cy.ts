76bf2777c5b4b92543215c2b432d47d4280a1953
Florian König <koenidv@gmail.com>
2022-11-08 18:33:10 +0100

    Set up component testing

---
+++ cypress/component/ChessBoard.cy.ts
@@ -0,0 +1,6 @@
+describe('ChessBoard.cy.ts', () => {
+  it('playground', () => {
+    // cy.mount()
+    // todo create component tests once e2e tests are working and board is its own component
+  })
+})
