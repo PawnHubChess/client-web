d1d8f9ba189f3e9a9ad6ed16de54b43cc56ad2f0
Florian König <koenidv@gmail.com>
2022-11-14 20:19:50 +0100

    Test message handler proxy function

--- tests/WebSocketConnection.test.ts
+++ tests/WebSocketConnection.test.ts
@@ -1,5 +1,6 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import { WebSocketConnection } from "$lib/chess/WebSocketConnection";
+import { json } from "stream/consumers";
 
 test("register message handler", () => {
   const connection = new WebSocketConnection();
@@ -24,8 +25,8 @@ test("register message handler", () => {
 });
 
 test("deregister message handler", () => {
-    const connection = new WebSocketConnection();
-    const handler = () => {
+  const connection = new WebSocketConnection();
+  const handler = () => {
     true;
   };
 
@@ -34,10 +35,34 @@ test("deregister message handler", () => {
   const destroy2 = connection.on("message-1", handler);
 
   destroy1();
-  
+
   const expectedHandlers = new Map<string, ((data: any) => void)[]>([
-      ["message-1", [handler]]
-    ]);
-    
-    expect(connection.messageHandlers).toEqual(expectedHandlers);
+    ["message-1", [handler]],
+  ]);
+
+  expect(connection.messageHandlers).toEqual(expectedHandlers);
+});
+
+test("uses correct message handler", () => {
+  // Cast class to any to access private methods
+  const connection = new WebSocketConnection() as any;
+  const handler1 = vi.fn();
+  const handler2 = vi.fn()
+  const handler3 = vi.fn()
+
+  connection.messageHandlers = new Map<string, ((data: any) => void)[]>([
+    ["message-1", [handler1, handler3]],
+    ["message-2", [handler2]],
+  ]);
+
+  const messageEvent = new MessageEvent("message", {
+    data: JSON.stringify({
+      type: "message-1",
+    }),
+  });
+
+  connection.handleMessage(messageEvent);
+  expect(handler1).toHaveBeenCalled();
+  expect(handler2).not.toHaveBeenCalled();
+  expect(handler3).toHaveBeenCalled();
 });
