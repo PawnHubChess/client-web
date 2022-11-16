1839ee9d2015c6351724fd05ec9cbadb09a11eba
Florian König <koenidv@gmail.com>
2022-11-17 00:05:01 +0100

    Use Netlify edge functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-netlify";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,14 +7,16 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [
 		preprocess({
-		  postcss: true,
-		}),
-	  ],
+			postcss: true
+		})
+	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: true
+		}),
 		alias: {
-			$components: './src/components',
+			$components: "./src/components"
 		}
 	}
 };
