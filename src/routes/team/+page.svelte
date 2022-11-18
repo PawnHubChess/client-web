f04828db0f4d754fc010f9be11c989751c4dcf02
Florian König <koenidv@gmail.com>
2022-11-18 14:33:12 +0100

    Add links in team page

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,54 +1,88 @@
 <main>
 	<h1 class="mt-8 text-center text-7xl font-bold">Meet the Team</h1>
 
-	<div class="grid grid-cols-6 mt-8">
-		<div class="flex flex-col items-center">
-			<img src="/team_calculator.png" alt="Calculator icon" />
-			<p class="text-3xl font-bold">Max</p>
-			<ul class="pt-3 text-center">
-				<li>Project Owner</li>
-				<li>Image Recognition</li>
-			</ul>
-		</div>
-		<div class="flex flex-col items-center">
-			<img src="/team_pawn.png" alt="Pawn icon" />
-			<p class="text-3xl font-bold">Paul</p>
-			<ul class="pt-3 text-center">
-				<li>Physical Interface</li>
-				<li>Board Client</li>
-			</ul>
-		</div>
-		<div class="flex flex-col items-center">
-			<img src="/team_bishop.png" alt="Bishop icon" />
-			<p class="text-3xl font-bold">Péter</p>
-			<ul class="pt-3 text-center">
-				<li>Phyical Interface</li>
-				<li>Robotics</li>
-			</ul>
-		</div>
-		<div class="flex flex-col items-center">
-			<img src="/team_horse.png" alt="Bishop icon" />
-			<p class="text-3xl font-bold">Noah</p>
-			<ul class="pt-3 text-center">
-				<li>3d Design</li>
-				<li>Robotics</li>
-			</ul>
-		</div>
-		<div class="flex flex-col items-center">
-			<img src="/team_queen.png" alt="Queen icon" />
-			<p class="text-3xl font-bold">Jorge</p>
-			<ul class="pt-3 text-center">
-				<li>Physical Interface</li>
-			</ul>
-		</div>
-		<div class="flex flex-col items-center">
-			<img src="/team_king.png" alt="King icon" />
-			<p class="text-3xl font-bold">Florian</p>
-			<ul class="pt-3 text-center">
-				<li>Web Client</li>
-				<li>Backend</li>
-				<li>DevOps</li>
-			</ul>
-		</div>
+	<div class="mt-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2">
+		<a
+			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
+			href="https://www.linkedin.com/in/maximilian-arnold-787ba5197/"
+			target="_blank"
+			rel="noreferrer">
+			<div class="flex flex-col items-center">
+				<img src="/team_calculator.png" alt="Calculator icon" class="h-48" />
+				<p class="text-3xl font-bold">Max</p>
+				<ul class="pt-3 text-center">
+					<li>Project Owner</li>
+					<li>Image Recognition</li>
+				</ul>
+			</div>
+		</a>
+		<a
+			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
+			href="https://github.com/PaulBueckhard/">
+			<div class="flex flex-col items-center" target="_blank" rel="noreferrer">
+				<img src="/team_pawn.png" alt="Pawn icon" class="h-48" />
+				<p class="text-3xl font-bold">Paul</p>
+				<ul class="pt-3 text-center">
+					<li>Physical Interface</li>
+					<li>Board Client</li>
+				</ul>
+			</div>
+		</a>
+		<a
+			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
+			href="https://www.linkedin.com/in/p%C3%A9ter-tam%C3%A1s-winkler-a61a05173/"
+			target="_blank"
+			rel="noreferrer">
+			<div class="flex flex-col items-center">
+				<img src="/team_bishop.png" alt="Bishop icon" class="h-48" />
+				<p class="text-3xl font-bold">Péter</p>
+				<ul class="pt-3 text-center">
+					<li>Phyical Interface</li>
+					<li>Robotics</li>
+				</ul>
+			</div>
+		</a>
+		<a
+			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
+			href="https://github.com/noah-frank/"
+			target="_blank"
+			rel="noreferrer">
+			<div class="flex flex-col items-center">
+				<img src="/team_horse.png" alt="Bishop icon" class="h-48" />
+				<p class="text-3xl font-bold">Noah</p>
+				<ul class="pt-3 text-center">
+					<li>3d Design</li>
+					<li>Robotics</li>
+				</ul>
+			</div>
+		</a>
+		<a
+			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
+			href="https://www.linkedin.com/in/jorge-peyroteo-guedes-69aa49167/"
+			target="_blank"
+			rel="noreferrer">
+			<div class="flex flex-col items-center">
+				<img src="/team_queen.png" alt="Queen icon" class="h-48" />
+				<p class="text-3xl font-bold">Jorge</p>
+				<ul class="pt-3 text-center">
+					<li>Physical Interface</li>
+				</ul>
+			</div>
+		</a>
+		<a
+			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
+			href="https://www.linkedin.com/in/koenidv/"
+			target="_blank"
+			rel="noreferrer">
+			<div class="flex flex-col items-center">
+				<img src="/team_king.png" alt="King icon" class="h-48" />
+				<p class="text-3xl font-bold">Florian</p>
+				<ul class="pt-3 text-center">
+					<li>Web Client</li>
+					<li>Backend</li>
+					<li>DevOps</li>
+				</ul>
+			</div>
+		</a>
 	</div>
 </main>
