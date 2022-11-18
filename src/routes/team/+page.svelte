b831df2d6c2f2c70117190f8d6de32d0d7bdb06a
Florian König <koenidv@gmail.com>
2022-11-18 14:12:52 +0100

    Update team page

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,3 +1,54 @@
 <main>
 	<h1 class="mt-8 text-center text-7xl font-bold">Meet the Team</h1>
+
+	<div class="grid grid-cols-6 mt-8">
+		<div class="flex flex-col items-center">
+			<img src="/team_calculator.png" alt="Calculator icon" />
+			<p class="text-3xl font-bold">Max</p>
+			<ul class="pt-3 text-center">
+				<li>Project Owner</li>
+				<li>Image Recognition</li>
+			</ul>
+		</div>
+		<div class="flex flex-col items-center">
+			<img src="/team_pawn.png" alt="Pawn icon" />
+			<p class="text-3xl font-bold">Paul</p>
+			<ul class="pt-3 text-center">
+				<li>Physical Interface</li>
+				<li>Board Client</li>
+			</ul>
+		</div>
+		<div class="flex flex-col items-center">
+			<img src="/team_bishop.png" alt="Bishop icon" />
+			<p class="text-3xl font-bold">Péter</p>
+			<ul class="pt-3 text-center">
+				<li>Phyical Interface</li>
+				<li>Robotics</li>
+			</ul>
+		</div>
+		<div class="flex flex-col items-center">
+			<img src="/team_horse.png" alt="Bishop icon" />
+			<p class="text-3xl font-bold">Noah</p>
+			<ul class="pt-3 text-center">
+				<li>3d Design</li>
+				<li>Robotics</li>
+			</ul>
+		</div>
+		<div class="flex flex-col items-center">
+			<img src="/team_queen.png" alt="Queen icon" />
+			<p class="text-3xl font-bold">Jorge</p>
+			<ul class="pt-3 text-center">
+				<li>Physical Interface</li>
+			</ul>
+		</div>
+		<div class="flex flex-col items-center">
+			<img src="/team_king.png" alt="King icon" />
+			<p class="text-3xl font-bold">Florian</p>
+			<ul class="pt-3 text-center">
+				<li>Web Client</li>
+				<li>Backend</li>
+				<li>DevOps</li>
+			</ul>
+		</div>
+	</div>
 </main>
