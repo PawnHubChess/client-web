426077fd266293a5b6e5aa63450a89761305d448
Florian König <32238636+koenidv@users.noreply.github.com>
2022-11-29 09:52:17 +0100

    Match players integration test with jsdom

--- tests/test.integration.ts
+++ tests/test.integration.ts
@@ -1,27 +1,22 @@
+// @vitest-environment jsdom
+
 import { expect, test, vi } from "vitest";
 import { WebSocketConnection } from "$lib/chess/WebSocketConnection";
 import {
-  board_fen,
-  client_id,
-  current_player_white,
-  reconnect_code,
+  debug_local_server,
 } from "$lib/store";
-import { get } from "svelte/store";
 
 
-test("send connection requests", () => {
-  assert(false)
-
+test("match players", async () => {
+  debug_local_server.set(true);
   const connection = new WebSocketConnection();
-  connection.send = vi.fn();
+  await connection.prepare();
+  const matchedSpy = vi.fn();
+  connection.on("matched", matchedSpy);
 
-  connection.sendConnectRequest("0987", "1234");
+  connection.sendConnectRequest("0000", "1234");
 
-  expect(connection.send).toHaveBeenCalledWith(
-    JSON.stringify({
-      type: "connect-attendee",
-      host: "0987",
-      code: "1234",
-    }),
-  );
+  await new Promise(r => setTimeout(r, 100));
+
+  expect(matchedSpy).toHaveBeenCalledOnce();
 });
