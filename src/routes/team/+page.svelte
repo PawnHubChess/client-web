856a0967360d3eeba3cb15645042738d84206672
Florian König <koenidv@gmail.com>
2023-03-13 17:15:23 +0100

    Update Team

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,24 +1,10 @@
 <main class="mb-8">
 	<h1 class="mt-8 text-center text-7xl font-bold">Meet the Team</h1>
 
-	<div class="mt-8 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-6">
+	<div class="mt-8 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-5">
 		<a
 			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
-			href="https://www.linkedin.com/in/maximilian-arnold-787ba5197/"
-			target="_blank"
-			rel="noreferrer">
-			<div class="flex flex-col items-center">
-				<img src="/team_calculator.webp" alt="Calculator icon" class="h-40 xl:h-48" />
-				<p class="text-3xl font-bold">Max</p>
-				<ul class="pt-3 text-center">
-					<li>Project Owner</li>
-					<li>Image Recognition</li>
-				</ul>
-			</div>
-		</a>
-		<a
-			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
-			href="https://github.com/PaulBueckhard/">
+			href="https://www.linkedin.com/in/paul-bückhard-7b71a726a/">
 			<div class="flex flex-col items-center" target="_blank" rel="noreferrer">
 				<img src="/team_pawn.webp" alt="Pawn icon" class="h-40 xl:h-48" />
 				<p class="text-3xl font-bold">Paul</p>
@@ -44,28 +30,28 @@
 		</a>
 		<a
 			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
-			href="https://github.com/noah-frank/"
+			href="https://www.linkedin.com/in/lennart-conrad-ywain-wennmachers-b8911b13b/"
 			target="_blank"
 			rel="noreferrer">
 			<div class="flex flex-col items-center">
 				<img src="/team_horse.webp" alt="Bishop icon" class="h-40 xl:h-48" />
-				<p class="text-3xl font-bold">Noah</p>
+				<p class="text-3xl font-bold">Lennart</p>
 				<ul class="pt-3 text-center">
-					<li>3d Design</li>
-					<li>Robotics</li>
+					<li>Product Management</li>
 				</ul>
 			</div>
 		</a>
 		<a
 			class="rounded-md bg-gray-50 pb-4 transition-all hover:bg-indigo-50"
-			href="https://www.linkedin.com/in/jorge-peyroteo-guedes-69aa49167/"
+			href="https://github.com/noah-frank/"
 			target="_blank"
 			rel="noreferrer">
 			<div class="flex flex-col items-center">
 				<img src="/team_queen.webp" alt="Queen icon" class="h-40 xl:h-48" />
-				<p class="text-3xl font-bold">Jorge</p>
+				<p class="text-3xl font-bold">Noah</p>
 				<ul class="pt-3 text-center">
-					<li>Physical Interface</li>
+					<li>3d Design</li>
+					<li>Robotics</li>
 				</ul>
 			</div>
 		</a>
